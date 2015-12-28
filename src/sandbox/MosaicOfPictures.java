845120e973f70e9d3a6a2c30238badb44743dd35
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2015-12-28 17:35:37 +0100

    cont

--- src/sandbox/MosaicOfPictures.java
+++ src/sandbox/MosaicOfPictures.java
@@ -10,12 +10,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.logging.Logger;
 
 import javax.imageio.ImageIO;
 
-public class MosaicOfPictures
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
+public class MosaicOfPictures extends AbstractApplication
 	{
 	private static final Logger LOG=Logger.getLogger("lindenb");
 	
@@ -45,139 +50,135 @@ public class MosaicOfPictures
 		return set;
 		}
 	
-	private void run(String[] args) throws Exception
+	@Override
+	protected void fillOptions(Options options) {
+		options.addOption(Option.builder("c").longOpt("width").desc("final image width").hasArg(true).build());
+		options.addOption(Option.builder("o").longOpt("out").desc("image output").hasArg(true).build());		
+		super.fillOptions(options);
+		}
+	
+	@Override
+	protected int execute(CommandLine cmd)
 		{
-		int image_size=400;
-		Set<String>  picts=null;
-		File outFile=null;
-		int optind=0;
-		while(optind< args.length)
-			{
-			if(args[optind].equals("-h") ||
-			   args[optind].equals("-help") ||
-			   args[optind].equals("--help"))
+			try {
+			int image_size=400;
+			Set<String>  picts=null;
+			File outFile=null;
+			
+			if(cmd.hasOption('c'))
 				{
-				System.err.println("Options:");
-				System.err.println(" -h help; This screen.");
-				System.err.println(" -c (width) final image size");
-				System.err.println(" -o (file) fileout.jpg");
-				return;
+				image_size= Integer.parseInt(cmd.getOptionValue('c'));
 				}
-			else if(args[optind].equals("-c"))
+			if(cmd.hasOption('o'))
 				{
-				image_size=Integer.parseInt(args[++optind]);
-				}
-			else if(args[optind].equals("-o"))
-				{
-				outFile=new File(args[++optind]);
-				}
-			else if(args[optind].equals("--"))
-				{
-				optind++;
-				break;
-				}
-			else if(args[optind].startsWith("-"))
-				{
-				System.err.println("Unknown option "+args[optind]);
-				return;
-				}
-			else 
-				{
-				break;
-				}
-			++optind;
-			}
-		
-		if(outFile==null)
-			{
-			System.err.println("No output file");
-			System.exit(-1);
-			}
-		
-		if(optind==args.length)
-			{
-			picts=readPictures(new BufferedReader(new InputStreamReader(System.in)));
-			}
-		else if(optind+1==args.length)
-			{
-			BufferedReader r=new BufferedReader(new FileReader(args[optind]));
-			picts=readPictures(r);
-			r.close();
-			}
-		else
-			{
-			System.err.println("Illegal number of arguments.");
-			System.exit(-1);
-			}
-		
-		
-		if(picts.isEmpty())
-			{
-			System.err.println("No images");
-			System.exit(-1);
-			}
-		int per_side=(int)Math.ceil(Math.sqrt(picts.size()));
-		double one_length =image_size/Math.ceil(Math.sqrt(picts.size()));
-		
-		
-		BufferedImage img=new BufferedImage(
-				image_size,
-				image_size,
-				BufferedImage.TYPE_INT_RGB
-				);
-		Graphics2D g=img.createGraphics();
-		g.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
-		g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-
-		int y=0;
-		int x=0;
-		int nReads=0;
-		for(String file:picts)
-			{
-			++nReads;
-			LOG.info(file+" "+nReads+"/"+picts.size());
-			BufferedImage img2=null;
-			if(file.startsWith("http://") || file.startsWith("https://") || file.startsWith("ftp://"))
-				{
-				img2=ImageIO.read(new URL(file));
+				outFile= new File(cmd.getOptionValue('o'));
 				}
 			else
 				{
-				img2=ImageIO.read(new File(file));
+				LOG.severe("No output file");
+				return -1;
 				}
 			
-			double w=img2.getWidth();
-			double h=img2.getHeight();
-			double ratio=w/h;
-		
-			while(w>one_length || h> one_length)
+			final List<String> args = cmd.getArgList();
+			
+			if(args.isEmpty())
 				{
-				w*=0.99999;
-				h=w/ratio;
+				picts=readPictures(new BufferedReader(new InputStreamReader(System.in)));
 				}
-			g.drawImage(
-					img2,
-					(int)(x*one_length+(one_length-(int)w)/2.0),
-					(int)(y*one_length+(one_length-(int)h)/2.0),
-					(int)w,
-					(int)h,
-					null);
-			
-			img2=null;
-			
-			x++;
-			if(x>=per_side)
+			else if(args.size()==1)
 				{
-				x=0;
-				++y;
+				BufferedReader r=new BufferedReader(new FileReader(args.get(0)));
+				picts=readPictures(r);
+				r.close();
 				}
+			else
+				{
+				LOG.severe("Illegal number of arguments.");
+				return -1;
+				}
+			
+			
+			if(picts.isEmpty())
+				{
+				LOG.severe("No images");
+				return -1;
+				}
+			int per_side=(int)Math.ceil(Math.sqrt(picts.size()));
+			double one_length =image_size/Math.ceil(Math.sqrt(picts.size()));
+			
+			
+			BufferedImage img=new BufferedImage(
+					image_size,
+					image_size,
+					BufferedImage.TYPE_INT_RGB
+					);
+			Graphics2D g=img.createGraphics();
+			g.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+			g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+	
+			int y=0;
+			int x=0;
+			int nReads=0;
+			for(String file:picts)
+				{
+				++nReads;
+				LOG.info(file+" "+nReads+"/"+picts.size());
+				BufferedImage img2 = null;
+				if( file.startsWith("http://") ||
+					file.startsWith("https://") ||
+					file.startsWith("ftp://"))
+					{
+					img2=ImageIO.read(new URL(file));
+					}
+				else
+					{
+					img2=ImageIO.read(new File(file));
+					}
+				if(img2==null)
+					{
+					LOG.severe("Cannot read "+file);
+					return -1;
+					}
+				double w=img2.getWidth();
+				double h=img2.getHeight();
+				double ratio=w/h;
+			
+				while(w>one_length || h> one_length)
+					{
+					w*=0.99999;
+					h=w/ratio;
+					}
+				g.drawImage(
+						img2,
+						(int)(x*one_length+(one_length-(int)w)/2.0),
+						(int)(y*one_length+(one_length-(int)h)/2.0),
+						(int)w,
+						(int)h,
+						null);
+				
+				img2=null;
+				
+				x++;
+				if(x>=per_side)
+					{
+					x=0;
+					++y;
+					}
+				}
+			g.dispose();
+			LOG.info("Saving");
+			ImageIO.write(img, "JPG", outFile);
+			return 0;
 			}
-		g.dispose();
-		LOG.info("Saving");
-		ImageIO.write(img, "JPG", outFile);
+		catch(Exception err)
+			{
+			err.printStackTrace();
+			LOG.severe(err.getMessage());
+			return -1;
+			}
 		}
 	public static void main(String[] args) throws Exception
 		{
-		new MosaicOfPictures().run(args);
+		new MosaicOfPictures().instanceMainWithExit(args);
 		}
 	}
