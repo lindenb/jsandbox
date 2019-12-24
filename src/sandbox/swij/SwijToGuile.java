ec2a800fe00355260f0884320c8ece9e69d2da6e
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2019-12-24 12:40:53 +0100

    cont

--- src/sandbox/swij/SwijToGuile.java
+++ src/sandbox/swij/SwijToGuile.java
@@ -18,7 +18,7 @@ private class GuileFun {
 	}
 	
 	String getGuileName() {
-	return this.fun.name.replace('_','-');
+	return this.fun.name.replace('_','-')+(this.fun.type.isBoolean()?"?":"");
 	}
 	
 	void print(PrintStream w) {
@@ -33,16 +33,19 @@ private class GuileFun {
 		}
 		w.println(") {");
 		if(!fun.type.isVoid()) {
-			w.println(fun.type.getCName()+" _ret;");
+			w.println(fun.type.getCName()+" _ret = "+fun.type.getDefaultValue()+":");
 			}
 		for(int i=0;i< fun.parameters.size();i++) {
 			final SwijParser.Param p = fun.parameters.get(i);
-			w.println(p.type.getCName()+" "+p.name+";");
-			}	
+			w.println(p.type.getCName()+" "+p.name+"="+p.type.getDefaultValue()+";");
+			}
 		for(int i=0;i< fun.parameters.size();i++) {
 			final SwijParser.Param p = fun.parameters.get(i);
 			if(p.type.isInt()) {
 				w.println("SCM_ASSERT (scm_is_integer(s_"+p.name+"),s_"+p.name+","+i+",\""+getGuileName()+"\");");
+				}
+			else if(p.type.isBoolean()) {
+				w.println("SCM_ASSERT (scm_is_bool(s_"+p.name+"),s_"+p.name+","+i+",\""+getGuileName()+"\");");
 				}
 			else if(p.type.isString()) {
 				w.println("SCM_ASSERT (scm_is_string(s_"+p.name+"),s_"+p.name+","+i+",\""+getGuileName()+"\");");
@@ -56,6 +59,9 @@ private class GuileFun {
 			final SwijParser.Param p = fun.parameters.get(i);
 			if(p.type.isInt()) {
 				w.println(p.name+"= scm_to_int(s_"+p.name+");");
+				}
+			else if(p.type.isBoolean()) {
+				w.println(p.name+"= scm_to_bool(s_"+p.name+");");
 				}
 			else if(p.type.isString()) {
 				w.println(p.name+"= scm_to_locale_string(s_"+p.name+");");
@@ -92,13 +98,16 @@ private class GuileFun {
 			w.println("return _ret2;");
 			w.println("}");
 			}
+		else if(fun.type.isBoolean()) {
+			w.println("return scm_from_bool(_ret);");
+			}
 		else if(fun.type.isInt()) {
 			w.println("return scm_from_int(_ret);");
 			}
 		else
 			{
 			w.println("if(_ret==NULL) return  SCM_UNDEFINED;");
-			w.println("return scm_from_pointer(_ret,_dispose_"+fun.type.getCName()+");");
+			w.println("return scm_from_pointer(_ret,_dispose_"+fun.type.getCName().replace("*","_ptr")+");");
 			}
 		w.println("}");
 	}
