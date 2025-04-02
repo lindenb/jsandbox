db4ac844cea99f86cd5bdf2d856972e1ed1b6c51
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2025-04-02 21:11:35 +0200

    cont

---
+++ src/main/java/sandbox/lang/StringWrapper.java
@@ -0,0 +1,40 @@
+package sandbox.lang;
+
+public class StringWrapper implements CharSequence {
+	private final String delegate;
+	public StringWrapper(final String s) {
+		this.delegate = s;
+		}
+	
+	@Override
+	public int hashCode() {
+		return delegate.hashCode();
+		}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj==this) return true;
+		if(obj==null || !(obj instanceof StringWrapper)) return false;
+		return this.delegate.equals(StringWrapper.class.cast(obj).delegate);
+		}
+	
+	@Override
+	public String toString() {
+		return delegate;
+		}
+
+	@Override
+	public int length() {
+		return delegate.length();
+	}
+
+	@Override
+	public char charAt(int index) {
+		return delegate.charAt(index);
+	}
+
+	@Override
+	public CharSequence subSequence(int start, int end) {
+		return delegate.subSequence(start, end);
+	}
+	}
--- src/main/java/sandbox/minilang1/MiniLang1Context.java
+++ src/main/java/sandbox/minilang1/MiniLang1Context.java
@@ -1,42 +1,156 @@
 package sandbox.minilang1;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import sandbox.lang.StringWrapper;
+
 public class MiniLang1Context {
-	public interface Function
+	static final int OP_DIVIDE   = 100;
+	static final int OP_MULTIPLY = 101;
+	static final int OP_PLUS = 102;
+	static final int OP_MINUS = 103;
+	static final int OP_NEGATE = 104;
+	static final int OP_VARIABLE_ASSIGN = 200;
+	static final int OP_WRAPPED = 300;
+	static final int OP_FUN_CALL = 400;
+	
+	
+	static abstract class ASTNode {
+		int type;
+		int depth=0;
+		ASTValue asASTValue() {
+			return ASTValue.class.cast(this);
+			}
+		}
+	private static class ASTBiNode extends ASTNode {
+		ASTNode left;
+		ASTNode right;
+		}
+	private static class ASTUnaryNode extends ASTNode {
+		ASTNode node;
+		}
+	private static class ASTValue extends ASTNode {
+		WrappedValue value;
+		}
+	private static class ASTFunctionCall extends ASTNode {
+		String funName;
+		List<ASTNode> list;
+		Map<String, ASTNode> map;
+		}
+	static class Variable extends StringWrapper {
+		Variable(final String s) {
+			super(s);
+			}
+		}
+	
+	abstract class Function
 		{
-		public Object apply(final List<Object> list,Map<String, Object> map);
+		final String funName;
+		List<ASTNode> list;
+		Map<String, ASTNode> map;
+		Function(final String funName) {
+			this.funName = funName;
+			}
+		void arity(int n) {}
+		public abstract Object eval(State state);
 		}
-	private final MiniLang1Context parent;
-	private final Map<String,Object> id2variable = new HashMap<>();
+	private static class State {
+		private  State parent;
+		private final Map<Variable,Object> id2variable = new HashMap<>();
+		private final Map<String,Function> functions = new HashMap<>();
+		}
+	
+	private static class WrappedValue {
+		Object o;
+		WrappedValue(Object o) {
+			this.o=o;
+		}
+		boolean isNil() {
+			return this.o==null;
+			}
+		}
+	
+	State state =new State();
+	
+	
+	
 	public MiniLang1Context() {
-		this.parent = null;
+		Function fun = new Function("sqrt") {
+			@Override
+			public Object eval(State state) {
+				arity(1);
+				Object o= MiniLang1Context.this.eval(state,this.list.get(0));
+				BigInteger bi=(BigInteger)o;
+				return bi.sqrt();
+				}
+			};
+		state.functions.put(fun.funName, fun);
 		}
-	public MiniLang1Context(MiniLang1Context parent) {
-		this.parent = parent;
+	
+	private void initNode(ASTNode n,int type) {
+		
 		}
-	public Object put(String key,Object value) {
-		this.id2variable.put(key, value);
+	
+	ASTNode wrapValue(Object o) {
+		ASTValue n=new ASTValue();
+		initNode(n,OP_WRAPPED);
+		n.value = new WrappedValue(o);
+		return n;
+		}
+	
+	ASTNode create(ASTNode left,int type,ASTNode right) {
+		ASTBiNode n=new ASTBiNode();
+		initNode(n,type);
+		n.left = left;
+		n.right = right;
+		return n;
+		}
+	ASTNode create(int type,ASTNode node) {
+		ASTUnaryNode n=new ASTUnaryNode();
+		initNode(n,type);
+		n.node = node;
+		return n;
+		}
+	Object put(Variable key,Object value) {
+		state.id2variable.put(key, value);
 		return value;
 		}
-	public Object plus(Object o1,Object o2) {
-		return null;
+
+	ASTNode createVariable(String name) {
+		return wrapValue(new Variable(name));
 		}
-	public Object minus(Object o1,Object o2) {
-		return null;
+	ASTNode call(String funName,List<ASTNode> array,Map<String, ASTNode> hash) {
+		ASTFunctionCall fun = new ASTFunctionCall();
+		initNode(fun,OP_FUN_CALL);
+		fun.funName = funName;
+		fun.list = array;
+		fun.map= hash;
+		return fun;
 		}
-	public Object divide(Object o1,Object o2) {
-		return null;
+	
+	Object eval(State state,String funName,List<ASTNode> array,Map<String, ASTNode> hash) {
+		throw new IllegalStateException(""+funName);
 		}
-	public Object multiply(Object o1,Object o2) {
-		return null;
-		}
-	public Object negate(Object o) {
-		return null;
-		}
-	public Object call(String funName,List<Object> array,Map<String, Object> hash) {
-		return null;
+	
+	Object eval(State state,ASTNode node) {
+		switch(node.type) {
+			case OP_WRAPPED: {
+				Object o= ASTValue.class.cast(node).value;
+				if(o!=null && o instanceof Variable ) {
+					o= state.id2variable.get(Variable.class.cast(o));
+					}
+				return o;
+				}	
+			case OP_FUN_CALL:
+				ASTFunctionCall fc = ASTFunctionCall.class.cast(node);
+				Function fun = state.functions.get(fc.funName);
+				fun.list = fc.list;
+				fun.map = fc.map;
+				return fun.eval(state);
+			default: throw new IllegalStateException(""+node.type);
+			}
 		}
 	}
