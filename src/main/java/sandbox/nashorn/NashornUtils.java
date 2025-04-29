a8003f4447db4c7decad4d5a7c2dcc5fd6e8700b
lindenb <plindenbaum@yahoo.fr>
2025-04-29 17:47:23 +0200

    cont

---
+++ src/main/java/sandbox/nashorn/NashornUtils.java
@@ -0,0 +1,19 @@
+package sandbox.nashorn;
+
+import java.util.Optional;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
+public class NashornUtils {
+public static final String NASHORN="nashorn";
+public static Optional<ScriptEngine> makeEngine() {
+	final ScriptEngineManager mgr=new  ScriptEngineManager();
+	final ScriptEngine scripEngine= mgr.getEngineByExtension(NASHORN);
+	return Optional.ofNullable(scripEngine);
+	}
+
+public static ScriptEngine makeRequiredEngine() {
+	return makeEngine().orElseThrow(()->new RuntimeException("Cannot get a javascript engine \""+NASHORN+"\""));
+	}
+}
